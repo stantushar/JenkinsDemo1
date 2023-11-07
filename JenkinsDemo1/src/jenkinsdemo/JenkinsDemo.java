abf6d93c0a6fff817cdc8fa1853f1b84b30989d3
Tushar Chaudhary <tushargeniusbuddy@gmail.com>
2023-11-07 22:52:28 +0530

    jenkins demo1

---
+++ JenkinsDemo1/src/jenkinsdemo/JenkinsDemo.java
@@ -0,0 +1,13 @@
+package jenkinsdemo;
+
+public class JenkinsDemo {
+
+	public static void main(String[] args) {
+		
+		printFunction("Tushar", "Chaudhari");
+	}
+	public static void printFunction(String firstName , String lastName)
+	{
+		System.out.println("Hello "+firstName + lastName+ ", Welcome to Avaloq DevOps World");
+	}
+}
